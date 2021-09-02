6ad3b0994eaf71a043efb002c891b2cfb8f04b33
BridgetGatsi <bridgetgatsi@gmail.com>
2021-09-02 21:19:21 +0200

    Fix errors

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,29 @@
 
 void print_number(int n)
 {
-	unsigned int i, j, ct;
+unsigned int i, j, ct;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = n * -1;
-	}
-	else
-	{
-		i = n;
-	}
+if (n < 0)
+{
+_putchar(45);
+i = n * -1;
+}
+else
+{
+i = n;
+}
 
-	j = i;
-	ct = 1;
+j = i;
+ct = 1;
 
-	while (j > 9)
-	{
-		j /= 10;
-		ct *= 10;
-	}
+while (j > 9)
+{
+j /= 10;
+ct *= 10;
+}
 
-	for (; ct >= 1; ct /= 10)
-	{
-		_putchar(((i / ct) % 10) + 48);
-	}
+for (; ct >= 1; ct /= 10)
+{
+_putchar(((i / ct) % 10) + 48);
+}
 }
