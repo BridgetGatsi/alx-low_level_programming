780776e2b2a79b5f168f4f1d8274a31c382a70b8
BridgetGatsi <bridgetgatsi@gmail.com>
2021-09-02 21:06:56 +0200

    my update is done now

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,46 +1,24 @@
-#include "holberton.h"
-
+#include <stdio.h>
+#include "main.h"
 
 /**
-
- * _isdigit - Shows 1 if the input is
-
- * a digit. Other cases show
-
- * 0
-
- *
-
- * @c: The character in ASCII code
-
- *
-
- * Return: 1 for lowercase character. 0 for the rest.
-
+ * _isdigit - start of program
+ * @c : int value
+ * Return: is 0 or 1
  */
 
 
 int _isdigit(int c)
-  
 {
-  
-	if (c >= 48 && c <= 57)
-	  
-	{
-	  
-		return (1);
-		
-	}
-	
-	else
-	  
-	{
-	  
-		return (0);
-		
-	}
-	
-	_putchar('\n');
-	
-}
+/*char ch;*/
+/*ch = c + '0';*/
 
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+int _putchar(char c);
 int _isupper(int c);
-int _putchar(char);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -13,5 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif 
